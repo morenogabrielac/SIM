6903c19e9c4543135b4b916eeeae44ec90ee832b
Gabriel Moreno <gabriel_2008@live.com.ar>
2023-04-10 00:50:21 -0300

    generador Aleatorio agregado.

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+from tkinter import *
+from tkinter import ttk 
+import service.generadorVariablesAleatorias as rng
+import src.tabla as tabla
+c = random.random()
+print(rng.distribucionExpNegativa(4))
+tabla
